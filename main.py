c557b0cb6827e748215debd004bf3f4cc6c11304
Goutham K B <goutham79007@gmail.com>
2023-09-23 22:12:43 +0530

    buisness logic

--- main.py
+++ main.py
@@ -16,18 +16,51 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/<user>',methods=['POST','GET'])
-def user(user):
-    session[request.form['username']] = request.form['password']
+@app.route('/signin' ,methods=["POST","GET"])
+def signin():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        with connection:
+            with connection.cursor() as cr:
+                cr.execute("SELECT * FROM users WHERE username = %s AND password = %s",(username,password))
+                user = cr.fetchone()
+                if user is None:
+                    flash('User does not exist')
+                else:
+                    flash('User logged in')
+                    session[username] = request.form[password]
+                    return redirect(url_for('dashboard'))
+    return render_template('signin.html')
 
-    with connection:
-        with connection.cursor() as cr:
-            cr.execute("SELECT * FROM users WHERE username = %s",(user,))
-            user = cr.fetchone()
-            if user is None:
-                flash('User does not exist')
-                return redirect(url_for('index'))
 
-    return render_template('user.html',user=user)
+@app.route('/signup',methods=["POST","GET"])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        with connection:
+            with connection.cursor() as cr:
+                cr.execute("INSERT INTO users (username,password) VALUES (%s,%s)",(username,password))
+                connection.commit()
+                flash('User created')
+                return redirect(url_for('signin'))
+            
+    else:
+            
+        return render_template('signup.html')
+
+@app.route('/dashboard',methods=["POST","GET"] )
+def dashboard():
+    if session['username'] is None:
+        return redirect(url_for('signin'))
+    
+    if request.method == 'POST':
+        startPoint = request.form.get('startPoint')
+        endPoint = request.form.get('endPoint')
+
+        data
+    
 
 
+    return render_template('dashboard.html')
